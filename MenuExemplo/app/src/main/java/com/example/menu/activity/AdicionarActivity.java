4304474450a33befe5afeb2365874ebbd93df7e0
Gabriel Andrade <gabriel.santana@gmail.com>
2020-10-29 01:33:11 -0300

    Edição concluída.

--- MenuExemplo/app/src/main/java/com/example/menu/activity/AdicionarActivity.java
+++ MenuExemplo/app/src/main/java/com/example/menu/activity/AdicionarActivity.java
@@ -18,11 +18,19 @@ import com.google.android.material.textfield.TextInputEditText;
 public class AdicionarActivity extends AppCompatActivity {
 
     private TextInputEditText editTarefa;
+    private Tarefa tarefaAtual;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar);
         editTarefa = findViewById(R.id.editNomeTarefa);
+
+        //Recuperar Tarefa
+        tarefaAtual = (Tarefa) getIntent().getSerializableExtra("tarefaSelecionada");
+        if(tarefaAtual != null){
+            editTarefa.setText(tarefaAtual.getNomeTarefa());
+        }
+
     }
 
     @Override
@@ -35,18 +43,41 @@ public class AdicionarActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-
+        TarefaDAO dao = new TarefaDAO(getApplicationContext());
         switch (id) {
             case R.id.itemSalvar:
-                String nomeTarefa = editTarefa.getText().toString();
-                if(!nomeTarefa.isEmpty()){
-                    TarefaDAO dao = new TarefaDAO(getApplicationContext());
-                    Tarefa t = new Tarefa();
-                    t.setNomeTarefa(nomeTarefa);
-                    dao.salvar(t);
-                    finish();
-                    //Intent mainActivity = new Intent(this, MainActivity.class);
-                    //startActivity(mainActivity);
+                if(tarefaAtual != null) {
+                    String nomeTarefa = editTarefa.getText().toString();
+                    if (!nomeTarefa.isEmpty()) {
+                        Tarefa tarefa = new Tarefa();
+                        tarefa.setNomeTarefa(nomeTarefa);
+                        tarefa.setId(tarefaAtual.getId());
+
+                        //atualizar
+                        if(dao.atualizar(tarefa)){
+                            Toast.makeText(getApplicationContext(),"Sucesso ao atualizar a tarefa",Toast.LENGTH_LONG);
+                            finish();
+                        } else {
+                            Toast.makeText(getApplicationContext(),"Erro ao atualizar a tarefa",Toast.LENGTH_LONG);
+                        }
+                    }
+
+                } else {
+                    String nomeTarefa = editTarefa.getText().toString();
+                    if (!nomeTarefa.isEmpty()) {
+
+                        Tarefa t = new Tarefa();
+                        t.setNomeTarefa(nomeTarefa);
+                        if(dao.salvar(t)){
+                            Toast.makeText(getApplicationContext(),"Sucesso ao salvar a tarefa",Toast.LENGTH_LONG);
+                            finish();
+                        } else {
+                            Toast.makeText(getApplicationContext(),"Erro ao salvar a tarefa",Toast.LENGTH_LONG);
+                        }
+
+                        //Intent mainActivity = new Intent(this, MainActivity.class);
+                        //startActivity(mainActivity);
+                    }
                 }
 
 
--- MenuExemplo/app/src/main/java/com/example/menu/helper/data/TarefaDAO.java
+++ MenuExemplo/app/src/main/java/com/example/menu/helper/data/TarefaDAO.java
@@ -43,7 +43,21 @@ public class TarefaDAO implements ITarefaDAO {
 
     @Override
     public boolean atualizar(Tarefa tarefa) {
-        return false;
+
+        ContentValues cv = new ContentValues();
+        cv.put("nome", tarefa.getNomeTarefa());
+
+        try{
+            String[] args = {tarefa.getId().toString()};
+            escreve.update(
+                    DbHelper.TABELA_TAREFAS,cv,"id = ?", args);
+            Log.i("INTO DB ","Tarefa atualizada com sucesso " );
+        } catch (Exception e){
+            Log.i("INTO DB ","Erro ao atualizada a tarefa: " + e.getMessage() );
+            return false;
+        }
+
+        return true;
     }
 
     @Override
